6b4442fccdb5985d10675332c42e0f75a8b4e0dc
linzi <1297656202@qq.com>
2023-04-23 12:43:56 +0800

    string类函数的笔记

--- 格式及基本程序/动态分配内存.cpp
+++ 格式及基本程序/动态分配内存.cpp
@@ -9,3 +9,22 @@ for(i=0;i<n;i++)
 
 void* free(void* p) 
 free(p1);
+
+二维的:
+动态分配一个n行，m列的二维数组。
+先动态分配一维数组
+p = (int **)malloc(sizeof(int *) * n);
+再分配二维
+for (i = 0; i < n; i++) {
+	p[i] = (int *)malloc(sizeof(int) * m);
+}
+
+或者:
+int row=3;
+int col=5;
+int** array=new int* [row];
+for(int i=0;i<row;i++)
+{
+    array[i]=new int[col];
+} 
+ 
